00c76d3fa3379f1b573e589b5b7a5a64147264b2
Michael Beirne <133065459+michaelbeirnee@users.noreply.github.com>
2026-06-09 01:39:33 +0000

    fix

---
+++ bin/CachingProxyApplication.java
@@ -0,0 +1,7 @@
+package com.example.cachingproxy; 
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+import java.nio.file.Files; 
+import java.nio.file.Paths;
--- bin/caching-proxy.js
+++ bin/caching-proxy.js
@@ -1,6 +1,7 @@
 //import Node's built-in http library so we can create a server
 import http from "http"; 
 
+//import node's
 import fs from "fs"; 
 import path from "path"; 
 
